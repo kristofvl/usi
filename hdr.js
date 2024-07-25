b0bf0bfd105a2011eca8d25e61d0f0b154ccd046
Kristof Van Laerhoven <kryztov@gmail.com>
2024-07-25 15:10:20 +0200

    Create hdr.js

---
+++ hdr.js
@@ -0,0 +1,6 @@
+let Header = `<div id="usi_header">
+ <a href="https://www.uni-siegen.de"><img src="unis.svg" width="250" style="z-index:9; position:relative; top:30px"></img></a>
+ <a href="https://ubicomp.eti.uni-siegen.de"><img src="logo.svg" width="250" style="z-index:1; position:absolute; top:47px; right:10px;"></img></a>
+ </div>`;
+
+document.getElementById("hdr").innerHTML = Header;
